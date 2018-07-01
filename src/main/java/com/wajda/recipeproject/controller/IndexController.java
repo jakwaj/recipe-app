0b7dcf3b3742eb9f8ee00dd60b8cf9edc02f8edf
Jakub <jakubwajda@hotmail.com>
2018-07-01 11:12:39 +0200

    Added index page and controller

---
+++ src/main/java/com/wajda/recipeproject/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.wajda.recipeproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"/", "/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+
+}
