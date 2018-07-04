f1c473fa9a84b1fb76622f797a3b158f9adaa242
Jakub <jakubwajda@hotmail.com>
2018-07-04 17:50:51 +0200

    WIP

--- src/main/java/com/wajda/recipeproject/service/RecipeServiceImpl.java
+++ src/main/java/com/wajda/recipeproject/service/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.wajda.recipeproject.service;
 
 import com.wajda.recipeproject.model.Recipe;
 import com.wajda.recipeproject.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -22,4 +24,6 @@ public class RecipeServiceImpl implements RecipeService {
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
+
+
 }
