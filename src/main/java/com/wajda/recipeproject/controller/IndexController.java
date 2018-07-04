29b0c3954e7908565d76578dd5358a7934830db5
Jakub <jakubwajda@hotmail.com>
2018-07-04 17:58:32 +0200

    Added @Data and @Slf4j

--- src/main/java/com/wajda/recipeproject/controller/IndexController.java
+++ src/main/java/com/wajda/recipeproject/controller/IndexController.java
@@ -2,12 +2,14 @@ package com.wajda.recipeproject.controller;
 
 import com.wajda.recipeproject.model.Recipe;
 import com.wajda.recipeproject.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Set;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -19,6 +21,8 @@ public class IndexController {
 
     @GetMapping({"/", "/index"})
     public String getIndexPage(Model model) {
+
+        log.debug("Getting index page");
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
